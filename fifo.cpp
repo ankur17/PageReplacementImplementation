c128bff015ac76578615fb881d80b7c6cbcc703a
ankur17 <anankur17@gmail.com>
2016-03-12 16:34:33 +0530

    FIFO C++ implementation

---
+++ fifo.cpp
@@ -0,0 +1,59 @@
+//Implementing FIFO page segmentation
+#include <stdio.h>
+#include <iostream>
+//#include <algorithm>
+#include <vector>
+using namespace std;
+//int ar[3];
+vector<int> v;
+void Print(std::vector<int> v){
+	for(std::vector<int>::iterator it = v.begin();it!=v.end();it++){
+		cout<<*it<<"\t";
+	}
+	//cout<<endl;
+}
+bool see(std::vector<int> v, int num){
+	for(std::vector<int>::iterator it = v.begin();it!=v.end();++it){
+		if (*it==num) {
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+int main(){
+	int falut=0,page=0,slen=0;
+	cout<<"Enter the size of page: ";
+	cin>>page;
+	cout<<"Size of string: ";
+	cin>>slen;
+	cout<<"Enter string: ";
+	int s[100];
+	for (int i=0;i<slen;i++) cin>>s[i];
+//	cout<<"Hello ji\n"<<endl;
+
+	
+	for(int i=0;i<slen;i++){
+		if (v.size()<page){
+			if (!see(v,s[i])){
+				v.insert(v.begin(),s[i]);
+				Print(v);
+				cout<<endl;
+			}else
+			Print(v);cout<<endl;				
+		}else{
+			if(!see(v,s[i])){
+				//v.push_back(s[i]);
+				//v.erase(v.begin());
+				v.insert(v.begin(),s[i]);
+				v.pop_back();
+				Print(v);
+				falut++;
+				cout<<"FAULT No."<<falut<<"\n";
+			}else
+			Print(v);cout<<endl;				
+		}
+	}
+	cout<<"\n\nNumber Of Faluts: "<<falut;
+	return 0;
+}
