0d3ebd8f9b62c7fd1b6bf79fabec0afba0391453
ankur17 <anankur17@gmail.com>
2016-03-13 21:46:10 +0530

    OPT Implementation

--- OPT.py
+++ OPT.py
@@ -1,19 +1,23 @@
 s = map(int,"2 3 4 2 1 3 7 5 4 3".split())
-#sta = list()
 ar =list()
-#ar = [2,3,4]
 opt = dict()
+fault = 0 
 def add(start):
+	high = 0
+	fault = 0 
+	tm = 0
 	ls = s[start+1:]
 	for a in ar:
 		try:
 			opt[a] = ls.index(a)
 		except Exception, e:
 			opt[a] = 999
-	print "OUT: " + str(max(opt))
-	return max(opt)
-#add(3)
-print ar
+	for p,q in opt.items():
+		if q > high:
+			high = q
+			tm = p
+	opt.pop(tm)
+	return tm
 i=0
 for a in s:
 	if len(ar)<3:
@@ -23,5 +27,8 @@ for a in s:
 		if a not in ar:
 			tm = add(i)
 			ar[ar.index(tm)] = a
-	print ar, opt,i
+			print "\t".join(map(str,ar)),"Falut"
+			continue
+	print "\t".join(map(str,ar))
+
 	i+=1
